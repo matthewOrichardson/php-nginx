c0c915bc95ff4fb5f049e19dd14fd1a273d8d3fc
Matt Richardson <matthewOrichardson@gmail.com>
2024-09-19 15:51:21 -0500

    added php favicon :)

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
-<?php
+<head profile="http://www.w3.org/2005/10/profile">
+<link rel="icon" 
+      type="image/png" 
+      href="https://www.php.net/images/logos/php-logo.svg"><?php
 echo phpinfo();
